4b7e354d6e1ab3152743ac4f7f33e2cc7a0eb450
Alex Ford <alxtford@gmail.com>
2018-03-19 19:23:13 +0000

    Correctly added Cloudant, and set up basic, insecure connection

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ var app = express();
 //create a server and pass in app as a request handler
 var serv = require('http').createServer(app); //Server-11
 
-//var Cloudant = require('@cloudant/cloudant');
+var Cloudant = require('cloudant');
 
+var me = '511ca238-fccd-486b-b4a8-3441a50531bc-bluemix'; // Set this to your own account
+var password = "84da1dc77eccb3f811f8218aad8178d1ba45fa462ab8751a49ba85b35efe1927";
 
+// Initialize the library with my account.
+var cloudant = Cloudant({account:me, password:password});
+
+cloudant.db.list(function(err, allDbs) { console.log('All my databases: %s', allDbs.join(', '))});
 
 //send a index.html file when a get request is fired to the given
 //route, which is ‘/’ in this case
