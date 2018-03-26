1aed1e771781c93503f3c04c22fd2f3399d266c7
Alex Ford <alxtford@gmail.com>
2018-03-26 20:46:42 +0100

    Made phaser-input local, and removed nineslice

--- client/main.js
+++ client/main.js
@@ -37,7 +37,7 @@ var nameRegistered;
 
 function init(){
   clientGame.plugins.add(PhaserInput.Plugin);
-  clientGame.plugins.add(PhaserNineSlice.Plugin);
+  //clientGame.plugins.add(PhaserNineSlice.Plugin);
 
   this.game.stage.disableVisibilityChange = true;
   this.game.stage.smoothed = false;
