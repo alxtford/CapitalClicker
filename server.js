2e7386a17ef8d8356fc1efd8842e21d8b5c34221
Alex Ford <alxtford@gmail.com>
2018-03-26 20:33:14 +0100

    Server now redirects to HTTPS if client joins on http

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 
 //https://github.com/cloudant/nodejs-cloudant
+//https://www.tonyerwin.com/2014/09/redirecting-http-to-https-with-nodejs.html
+
 
 //import express.js
 var express = require('express');
 //assign it to variable app
 var app = express();
+
+
 //create a server and pass in app as a request handler
 var serv = require('http').createServer(app); //Server-11
 
@@ -44,7 +48,27 @@ var cloudant = Cloudant({account:me, password:password, maxAttempt: 5, plugins: 
   });
 
   //listen on port 2000
-  serv.listen(process.env.PORT || 2000);
+  serv.listen(process.env.PORT || process.env.VCAP_APP_PORT || 2000);
+
+  // Enable reverse proxy support in Express. This causes the
+  // the "X-Forwarded-Proto" header field to be trusted so its
+  // value can be used to determine the protocol. See
+  // http://expressjs.com/api#app-settings for more details.
+  app.enable('trust proxy');
+
+  // Add a handler to inspect the req.secure flag (see
+  // http://expressjs.com/api#req.secure). This allows us
+  // to know whether the request was via http or https.
+  app.use (function (req, res, next) {
+          if (req.secure) {
+                  // request was via https, so do no special handling
+                  next();
+          } else {
+                  // request was via http, so redirect to https
+                  res.redirect('https://' + req.headers.host + req.url);
+          }
+  });
+
   console.log("Server started.");
 
   process.on('exit', function() {
